daac9831bdba296dcff304131566d915b7214264
Alessi181 <tomasalessi@gmail.com>
2023-11-22 11:47:15 -0300

    feat: first script

--- src/scripts/data_preprocessing.py
+++ src/scripts/data_preprocessing.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 def preprocessing(data_dataframe):
     data_dataframe = data_dataframe.drop(["PESSOA_PIPEDRIVE_id_person_recommendation","PESSOA_PIPEDRIVE_Recebe Comunicados?", "PESSOA_PIPEDRIVE_Interesses", "PESSOA_PIPEDRIVE_Pontos de Atenção", "FUNIL_ONBOARDING_PIPEDRIVE_id_label"], axis=1)
+    return data_dataframe
