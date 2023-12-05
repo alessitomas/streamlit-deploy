4df2f53c0a7f9110a6252d93c341d246f03ac4fd
st4pzz <ramellajr@gmail.com>
2023-12-05 20:33:41 -0300

    feat : script data_to_deploy + model + database

--- api/app.py
+++ api/app.py
@@ -3,6 +3,8 @@ import joblib
 from ..src.scripts.data_preprocessing import preprocessing
 from dotenv import load_dotenv
 import os
+from flask_sqlalchemy import SQLAlchemy
+
 
 from ..src.scripts.data_feature_engineering import feature_engineering
 
@@ -11,9 +13,13 @@ load_dotenv()
 API_KEY_ANA = os.getenv('API_KEY_ANA')
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///log_Ana_Health.db'
+db = SQLAlchemy(app)
+
 model = joblib.load('./src/scripts/SVR_model.joblib')
 
-def verificar_api_key():
+def verificar_api_key():    
     api_key = request.headers.get('X-API-KEY')
     if api_key == API_KEY_ANA:
         return True
@@ -22,12 +28,16 @@ def verificar_api_key():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    from model import Log
     if not verificar_api_key():
         return jsonify({'error': 'Acesso negado'}), 403
     
     dados = request.json
     dados_preprocessados= preprocessing(dados)
     dados_feature = feature_engineering(dados_preprocessados)
+    novo_log = Log(data=str(dados))
+    db.session.add(novo_log)
+    db.session.commit()
     predicao = model.predict(dados_feature)
     return jsonify({'predicao': predicao.tolist()})
 
--- src/scripts/data_to_deploy.py
+++ src/scripts/data_to_deploy.py
@@ -1,6 +1,41 @@
 import pandas as pd
 from joblib import dump
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVR
+from sklearn.model_selection import GridSearchCV
 
-df = pd.read_csv("../notebooks/data/data-engineering.csv")
 
-dump(df, 'SVR_model.joblib')
+data = pd.read_csv("../scripts/data-engineering.csv")
+
+for indice, linha in data['status_won'].items():
+  if linha == 1:
+    data.drop(indice, inplace=True)
+
+data.reset_index(drop=True, inplace=True)
+
+X = data.drop(columns=['stay_time'], axis=1)
+y = data['stay_time']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.25,
+    random_state=42,
+)
+
+pipe_svr = Pipeline([
+    ("svr", SVR()),
+])
+
+parametros_grid_svr = {
+    'svr__kernel': ['linear', 'rbf'],
+    'svr__C': [0.1, 1, 10],
+    'svr__epsilon': [0.1, 0.2, 0.5],
+}
+
+modelo_grid = GridSearchCV(pipe_svr, parametros_grid_svr, cv=5, scoring='neg_mean_squared_error', verbose=1, n_jobs=-1)
+modelo_grid.fit(X_train, y_train)
+
+
+dump(modelo_grid, 'SVR_model.joblib')
