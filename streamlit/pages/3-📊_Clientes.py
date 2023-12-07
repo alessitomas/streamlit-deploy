42e7d6f9134e3fd7ba8dc461be3924dcad69613b
WeeverAlex <awever4000@gmail.com>
2023-12-07 18:39:18 -0300

    deixando os graficos dos clientes em colunas

--- streamlit/pages/3-📊_Clientes.py
+++ streamlit/pages/3-📊_Clientes.py
@@ -11,6 +11,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'u
 
 from helper_functions import get_data_from_mongo, plot_graphic_4, plot_graphic_5,plot_graphic_7, plot_graphic_8, check_authentication, logged_out_option
 
+# Configure o layout da página para 'wide'
+st.set_page_config(layout='wide')
+
 check_authentication()
 logged_out_option()
 
@@ -18,15 +21,18 @@ url = os.getenv('URL_DB')
 db_name = "AnaHealth"
 collection_name_dataset = "Dataset"
 
-
 df = get_data_from_mongo(url, db_name, collection_name_dataset)
 
 st.title("Vizualizações sobre os dados")
 
+# Crie duas colunas
+col1, col2 = st.columns(2)
 
-#st.write("Entrada e saída de clientes a cada mês")
+# Plot gráficos nas colunas
+with col1:
+    plot_graphic_4(df)
+    plot_graphic_5(df)
 
-plot_graphic_4(df)
-plot_graphic_7(df)
-plot_graphic_5(df)
-plot_graphic_8(df)
+with col2:
+    plot_graphic_7(df)
+    plot_graphic_8(df)
