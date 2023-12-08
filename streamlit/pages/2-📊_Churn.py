cb5240d89734f98e2fc27f700574d05823d88c46
WeeverAlex <awever4000@gmail.com>
2023-12-08 16:02:08 -0300

    criando filtro para escolher qual grafico voce quer ver

--- streamlit/pages/2-📊_Churn.py
+++ streamlit/pages/2-📊_Churn.py
@@ -17,31 +17,33 @@ df = get_data_from_mongo(url, db_name, collection_name_dataset)
 
 st.title("Vizualizações sobre os dados")
 
-# Adicionar uma barra lateral
-option = st.sidebar.selectbox(
-    'Escolha um gráfico',
-    ('Todos','Entrada e Saída de Assinaturas por Ano', 'Entrada e Saída de Assinaturas por Mês', 'Tendências de Churn por Mês', 'Motivos de Encerramento de Contrato de Assinatura', 'Tem Canal de Preferência', 'Tipo de contato preferido'))
+# Add a multi-select box in the sidebar with all options selected by default
+options = st.sidebar.multiselect(
+    'Escolha os gráficos para comparar',
+    ('Todos','Entrada e Saída de Assinaturas por Ano', 'Entrada e Saída de Assinaturas por Mês', 'Tendências de Churn por Mês', 'Motivos de Encerramento de Contrato de Assinatura', 'Tem Canal de Preferência', 'Tipo de contato preferido'),
+    default=['Entrada e Saída de Assinaturas por Ano', 'Entrada e Saída de Assinaturas por Mês', 'Tendências de Churn por Mês', 'Motivos de Encerramento de Contrato de Assinatura', 'Tem Canal de Preferência', 'Tipo de contato preferido'])
 
 check_authentication()
 logged_out_option()
 
-# Exibir o gráfico selecionado
-if option == 'Todos':
-    plot_graphic_10(df)
-    plot_graphic_1(df)
-    plot_graphic_2(df)
-    plot_graphic_3(df)
-    plot_graphic_6(df)
-    plot_graphic_9(df)
-elif option == 'Entrada e Saída de Assinaturas por Ano':
-    plot_graphic_10(df)
-elif option == 'Entrada e Saída de Assinaturas por Mês':
-    plot_graphic_1(df)
-elif option == 'Tendências de Churn por Mês':
-    plot_graphic_2(df)
-elif option == 'Motivos de Encerramento de Contrato de Assinatura':
-    plot_graphic_3(df)
-elif option == 'Tem Canal de Preferência':
-    plot_graphic_6(df)
-elif option == 'Tipo de contato preferido':
-    plot_graphic_9(df)
+# Display the selected graphs
+for option in options:
+    if option == 'Todos':
+        plot_graphic_1(df)
+        plot_graphic_2(df)
+        plot_graphic_3(df)
+        plot_graphic_6(df)
+        plot_graphic_9(df)
+        plot_graphic_10(df)
+    elif option == 'Entrada e Saída de Assinaturas por Ano':
+        plot_graphic_10(df)
+    elif option == 'Entrada e Saída de Assinaturas por Mês':
+        plot_graphic_1(df)
+    elif option == 'Tendências de Churn por Mês':
+        plot_graphic_2(df)
+    elif option == 'Motivos de Encerramento de Contrato de Assinatura':
+        plot_graphic_3(df)
+    elif option == 'Tem Canal de Preferência':
+        plot_graphic_6(df)
+    elif option == 'Tipo de contato preferido':
+        plot_graphic_9(df)
