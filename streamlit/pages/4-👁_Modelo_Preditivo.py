b4462c421a7045324877c0381bebdaeef4c7cf05
Alessi181 <tomasalessi@gmail.com>
2023-12-10 21:12:44 -0300

    feat: updating strwamlit graphics

--- streamlit/pages/4-👁_Modelo_Preditivo.py
+++ streamlit/pages/4-👁_Modelo_Preditivo.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.discriminant_analysis import StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-
+from kneed import KneeLocator, DataGenerator as dg
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
@@ -16,11 +16,13 @@ import sys
 import os
 import streamlit as st
 
+
 # Append the utils directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
 sns.set(style="whitegrid")  # Set style to 'whitegrid' for better visibility
 
 from helper_functions import get_data_from_mongo,check_authentication, logged_out_option
+
 url = st.secrets['URL_DB']
 db_name = "AnaHealth"
 collection_name_dataset = "Dataset"
@@ -73,6 +75,8 @@ data.drop(columns=["PESSOA_PIPEDRIVE_id_person", "PESSOA_PIPEDRIVE_id_gender","P
 data = pd.get_dummies(data,columns=["FUNIL_ASSINATURA_PIPEDRIVE_status"], prefix='assinatura_status')
 data = pd.get_dummies(data,columns=['FUNIL_ASSINATURA_PIPEDRIVE_lost_reason'], prefix='assinatura_lost_reason')
 
+st.markdown("### Método Elbow para Determinar o Número de Clusters")
+
 for indice, linha in data['assinatura_status_lost'].items():
   if linha != 1:
     data.drop(indice, inplace=True)
@@ -83,80 +87,99 @@ data = data.drop(columns=['_id'])
 scaler = StandardScaler()
 data_scaled = scaler.fit_transform(data)
 
-inertia = []
-for k in range(1, 11):
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    kmeans.fit(data_scaled)
-    inertia.append(kmeans.inertia_)
+# Generate concave increasing data
+X, y = dg.concave_increasing()
 
-st.markdown("### Método Elbow para Determinar o Número de Clusters")
-# Elbow method plot
-plt.figure(figsize=(10, 6))
-plt.plot(range(1, 11), inertia, marker='o', color='blue')
-plt.title('Método Elbow para Determinar o Número de Clusters', fontsize=16)
-plt.xlabel('Número de Clusters', fontsize=14)
-plt.ylabel('Inércia', fontsize=14)
-st.pyplot(plt.gcf())  # Display the plot in Streamlit
-plt.clf()  # Clear the current figure
+# Find the knee
+kl = KneeLocator(X, y, curve="concave")
 
+# Create a plot
+fig, ax = plt.subplots()
+ax.plot(X, y, label="Data")
+ax.scatter([kl.knee], [kl.knee_y], color="red", label="Knee Point", zorder=5)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_title("Knee Plot")
+ax.legend()
+ax.grid(True)
+
+# Display the plot in Streamlit
+st.pyplot(fig)
+
+st.markdown("### Distribuição do Tempo de Permanência por Cluster (conjunto de treinamento)")
+X = data.drop(columns=['stay_time'])  
+y = data['stay_time']  
+
+
+# Your existing code with slight modifications for Streamlit
 X = data.drop(columns=['stay_time'])  
 y = data['stay_time']  
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-kmeans = KMeans(n_clusters=8, random_state=42)
+    
+kmeans = KMeans(n_clusters=2, random_state=42)
 X_train['cluster'] = kmeans.fit_predict(X_train)
-
 X_test['cluster'] = kmeans.predict(X_test)
 
 silhouette_train = silhouette_score(X_train, X_train['cluster'])
 silhouette_test = silhouette_score(X_test, X_test['cluster'])
-
-print(f"Silhouette Score (Training): {silhouette_train}")
-print(f"Silhouette Score (Testing): {silhouette_test}")
-
+    
 ari_train = adjusted_rand_score(y_train, X_train['cluster'])
 ari_test = adjusted_rand_score(y_test, X_test['cluster'])
 
-print(f"Adjusted Rand Index (Training): {ari_train}")
-print(f"Adjusted Rand Index (Testing): {ari_test}")
+# Plotting for Streamlit
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.boxplot(x='cluster', y='stay_time', data=X_train.join(data['stay_time']), ax=ax, palette="Set2")
+ax.set_title('Distribuição do Tempo de Permanência por Cluster (conjunto de treinamento)')
+ax.set_xlabel('Cluster')
+ax.set_ylabel('Tempo de Permanência')
+st.pyplot(fig)
 
-st.markdown("### Distribuição do Tempo de Permanência por Cluster (conjunto de treinamento)")
-# Boxplot for training data
-plt.figure(figsize=(10, 6))
-sns.boxplot(x='cluster', y=data['stay_time'], data=X_train, palette='viridis')
-plt.title('Distribuição do Tempo de Permanência por Cluster (conjunto de treinamento)', fontsize=16)
-plt.xlabel('Cluster', fontsize=14)
-plt.ylabel('Tempo de Permanência', fontsize=14)
-st.pyplot(plt.gcf())  # Display the plot in Streamlit
-plt.clf()  # Clear the current figure
-
-st.markdown("### Distribuição do Tempo de Permanência por Cluster (conjunto de teste)")
-# Boxplot for testing data
-plt.figure(figsize=(10, 6))
-sns.boxplot(x='cluster', y=data['stay_time'], data=X_test, palette='viridis')
-plt.title('Distribuição do Tempo de Permanência por Cluster (conjunto de teste)', fontsize=16)
-plt.xlabel('Cluster', fontsize=14)
-plt.ylabel('Tempo de Permanência', fontsize=14)
-st.pyplot(plt.gcf())  # Display the plot in Streamlit
-plt.clf()  # Clear the current figure
-
-X = data.drop(columns=['stay_time'])
-y = data['stay_time']
-
-pca = PCA(n_components=2) 
-X_pca = pca.fit_transform(X)
-
-pca_df = pd.DataFrame(X_pca, columns=['pc1', 'pc2'])
-pca_df['target'] = y
-
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.boxplot(x='cluster', y='stay_time', data=X_test.join(data['stay_time']), ax=ax, palette="Set3")
+ax.set_title('Distribuição do Tempo de Permanência por Cluster (conjunto de teste)')
+ax.set_xlabel('Cluster')
+ax.set_ylabel('Tempo de Permanência')
+st.pyplot(fig)
 
 st.markdown("### PCA scatterplot")
 
-# PCA scatterplot
-plt.figure(figsize=(18, 14))
-sns.scatterplot(x='pc1', y='pc2', hue='target', data=pca_df, palette='coolwarm')
-plt.title('PCA aplicado no dataset', fontsize=20)
-plt.xlabel('Componente principal 1', fontsize=18)
-plt.ylabel('Componente principal 2', fontsize=18)
-st.pyplot(plt.gcf())  # Display the plot in Streamlit
+
+kmeans = KMeans(n_clusters=2, random_state=42)
+X_train['cluster'] = kmeans.fit_predict(X_train.drop(columns=['cluster']))
+X_test['cluster'] = kmeans.predict(X_test.drop(columns=['cluster']))
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train.drop(columns=['cluster']))
+X_test_pca = pca.transform(X_test.drop(columns=['cluster']))
+
+explained_variance_ratio = pca.explained_variance_ratio_
+
+
+pca_df_train = pd.DataFrame(X_train_pca, columns=['pc1', 'pc2'])
+pca_df_train['cluster'] = X_train['cluster']
+pca_df_train['target'] = y_train
+
+pca_df_test = pd.DataFrame(X_test_pca, columns=['pc1', 'pc2'])
+pca_df_test['cluster'] = X_test['cluster']
+pca_df_test['target'] = y_test
+
+cluster_labels = {0: 'Cluster 0', 1: 'Cluster 1'}
+pca_df_train['cluster'] = pca_df_train['cluster'].map(cluster_labels)
+pca_df_test['cluster'] = pca_df_test['cluster'].map(cluster_labels)
+
+# Plot para o conjunto de treinamento
+fig, ax = plt.subplots(figsize=(18, 14))
+sns.scatterplot(x='pc1', y='pc2', hue='cluster', data=pca_df_train, palette='Set1', s=100, ax=ax)
+ax.set_title('PCA aplicado nos resultados do cluster (Conjunto de Treinamento)')
+ax.set_xlabel('Componente principal 1')
+ax.set_ylabel('Componente principal 2')
+st.pyplot(fig)
+
+# Plot para o conjunto de teste
+fig, ax = plt.subplots(figsize=(18, 14))
+sns.scatterplot(x='pc1', y='pc2', hue='cluster', data=pca_df_test, palette='Set1', s=100, ax=ax)
+ax.set_title('PCA aplicado nos resultados do cluster (Conjunto de Teste)')
+ax.set_xlabel('Componente principal 1')
+ax.set_ylabel('Componente principal 2')
+st.pyplot(fig)
